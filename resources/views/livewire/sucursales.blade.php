de90e67f465ce9e504f6ac5a043bcabd579a18c3
David <ramzdave@gmail.com>
2021-02-25 13:05:35 -0600

    antes de bootstrap

--- resources/views/livewire/sucursales.blade.php
+++ resources/views/livewire/sucursales.blade.php
@@ -1,3 +1,4 @@
 <div>
     <h1>Desde vista componente Sucursales</h1>
+    <button class="btn btn-sm btn-success">Aceptar</button>
 </div>
--- resources/views/sucursal.blade.php
+++ resources/views/sucursal.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sucursales</title>
-    @livewireStyles
-</head>
-<body>
+@extends('layouts/main')
+
+@section('content')
     <livewire:sucursales/>
-    @livewireScripts
-</body>
-</html>
+@endsection
+    
