b56c8fb83000b0027c91ecc9b7586287863e1585
David <ramzdave@gmail.com>
2021-02-25 22:08:30 -0600

    componente sucursales

--- resources/views/livewire/sucursales.blade.php
+++ resources/views/livewire/sucursales.blade.php
@@ -1,26 +1,31 @@
 <div>
-    <div class="card border-secondary mb-3" style="max-width: 20rem;">
-        <div class="card-header">Sucursales</div>
+    <div class="card border-secondary  bg-dark mb-3" style="max-width: 20rem;">
+        <div class="card-header bg-dark">Sucursales</div>
         <div class="card-body">
             <form class="form-inline" wire:submit.prevent="store">
                 <div class="form-group mb-1">
                   <input type="text" class="form-control form-control-sm" wire:model="sucursal" placeholder="nombre">
                 </div>
                 <button type="submit" class="btn btn-sm btn-outline-success ml-1">Guardar</button>
-              </form>
+            </form>
               <br>
-              @error('sucursal') <span class="error">{{ $message }}</span> @enderror
-            <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Cras justo odio
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Dapibus ac facilisis in
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Morbi leo risus
-                </li>
-              </ul>
+              @error('sucursal') <span class="text-danger">{{ $message }}</span> @enderror
+
+              <hr>
+              <table class="table table-sm table-hover">
+                <tbody>
+                  <tr>
+                    <td>Column content</td>
+                  </tr>
+                </tbody>
+              </table>
+
         </div>
       </div>
+
+
+
+
+
+
 </div>
--- resources/views/sucursal.blade.php
+++ resources/views/sucursal.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row mt-3">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <livewire:sucursales/>
         </div>
         <div class="col-md-4"></div>
